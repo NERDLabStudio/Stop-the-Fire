85312d856376e9bdc4e33c03bf375bc79f209610
Clay Ewing <c.ewing@miami.edu>
2021-02-03 14:29:46 -0500

    Initial commit

---
+++ Assets/Game.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    public GameObject fire;
+    public GameObject helicopter;
+    public List<Tile> burningTiles;
+    public int phase = 0; //0 = place starting fire, 1 = placing helicopters
+    public int helicoptersLeft = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        burningTiles = new List<Tile>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlaceFire(Tile selectedTile, bool startingTile = false)
+    {
+       if(startingTile) {
+            selectedTile.isStartingTile = true;
+       }
+        Instantiate(fire, selectedTile.transform);
+        selectedTile.safe = false;
+        selectedTile.burnt = true;
+        selectedTile.state = 2;
+//        burningTiles.Add(selectedTile);
+    }
+
+    public void ClearBurningTiles()
+    {
+        burningTiles.Clear();
+    } 
+
+    public void PlaceHelicopter(Tile selectedTile)
+    {
+        if (helicoptersLeft > 0)
+        {
+            Debug.Log("Placing Helicopter!");
+            helicoptersLeft--;
+            selectedTile.helicopter = true;
+            selectedTile.safe = true;
+            selectedTile.helicopterObj = Instantiate(helicopter, selectedTile.transform);
+
+        }
+
+        if (helicoptersLeft <= 0)
+        {
+            phase = 1;
+            helicoptersLeft = 2;
+            //check containment
+            Debug.Log("No Helicopters Left, Checking Containment of Fires");
+            for (int i = 0; i < burningTiles.Count; i++) {
+                burningTiles[i].CheckFireContainment();
+            }
+        }
+    }
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+struct TouchingTile
+{
+    public GameObject obj;
+    public int state; //0 unused, 1 safe, 2 burned
+
+}
+public class Tile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int state; //0 unused, 1 safe, 2 burned
+    public bool burnt = false;
+    public bool safe = false;
+    public bool isStartingTile = false;
+    public bool helicopter = false;
+    public Tile[] pathways;
+    public Game game;
+    public GameObject helicopterObj;
+   
+
+    void Start()
+    {
+        game = FindObjectOfType<Game>();
+    }
+
+    private void OnMouseOver()
+    {
+         {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (game.phase == 1)
+                {
+                    //placing helicopters
+                    game.PlaceHelicopter(this);
+                }
+
+                if (game.phase == 0)
+                {
+                    //placing starting fire
+                    Debug.Log("Starting Fire!");
+                    game.PlaceFire(this, true);
+                    game.phase = 1;
+                }
+            }
+        }
+    }
+
+    public void SetHelicopter(GameObject h)
+    {
+        helicopter = h;
+    }
+    public void RemoveHelicopter()
+    {
+        Debug.Log("Destroying Helicopter " + helicopterObj);
+        Destroy(helicopterObj);
+    }
+    public void CheckFireContainment()
+    {
+        for (int i = 0; i < pathways.Length; i++) {
+            if (pathways[i].helicopter)
+            {
+                //has helicopter
+                Debug.Log("Has a Helicopter, Safe!");
+                pathways[i].safe = true;
+                pathways[i].RemoveHelicopter();
+            }
+            else if (pathways[i].burnt)
+            {
+                //pathway is already burned
+                Debug.Log("This path is already burned");
+            }
+            else if (pathways[i].safe)
+            {
+                Debug.Log("This tile (" + gameObject.name + ") is already safe!");
+            }
+            else
+            {
+                //not safe
+                Debug.Log("This tile isn't safe, burning");
+                game.PlaceFire(pathways[i]);
+                pathways[i].burnt = true;
+                pathways[i].safe = false;
+            }
+        }
+
+    }
+
+    public bool isSafe(Tile t)
+    {
+        return t.safe;
+    }
+    
+    public bool isAccessible(Tile t)
+    {
+        return !t.burnt;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
